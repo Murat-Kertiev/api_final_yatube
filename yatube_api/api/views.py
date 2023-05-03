64e9db5ea235225c88432c622b2b7fb631b59290
Кертиев Мурат <kertiev1107gmail.com>
2023-05-03 18:34:06 +0300

    Review 1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,56 @@
-# TODO:  Напишите свой вариант
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import (CommentSerializer, FollowSerializer,
+                             GroupSerializer, PostSerializer)
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from posts.models import Group, Post
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('group',)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=self.get_post())
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('user__username', 'following__username')
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,22 +5,79 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User,
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True,)
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        upload_to='posts/',
+        null=True,
+        blank=True,
+    )
+    group = models.ForeignKey(
+        'Group',
+        related_name='posts',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
-    def __str__(self):
-        return self.text
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название группы',
+        help_text='Введите название тематической группы',
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Номер группы',
+        help_text='Укажите порядковый номер группы',
+    )
+    description = models.TextField(
+        max_length=200,
+        verbose_name='Описание группы',
+        help_text='Добавьте текст описания группы',
+    )
 
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    text = models.TextField(max_length=300,)
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        auto_now_add=True,
+        db_index=True,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='follower',
+        on_delete=models.CASCADE,
+    )
+    following = models.ForeignKey(
+        User,
+        related_name='following',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                name='unique_follow',
+                fields=('user', 'following')
+            ),
+        )
